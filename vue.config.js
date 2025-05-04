6a1f97596df403d17f605812fcc0b096c761281c
Andrej-Novik <avacod222@gmail.com>
2025-05-04 17:33:06 +0300

    clear project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require("sass"),
+        additionalData: `
+        @import "@/assets/styles/breakpoints.scss";
+        @import "@/assets/styles/colors.scss";`,
+      },
+    },
+  },
 });
